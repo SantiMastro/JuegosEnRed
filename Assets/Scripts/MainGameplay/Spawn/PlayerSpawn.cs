b849728e5baf7e329d0408617986700bc3c51eb6
luqui1004 <lucascoceres0@gmail.com>
2024-11-10 04:54:48 -0300

    EXPERIMENTAL 7
    Se intentó cambiar el color a cada personaje segun se une, está comentado en el script de SpawnPlayer, no funciona porque como el jugador utiliza animaciones no se puede cambiar el color del spriterenderer, luego lo vemos

--- Assets/Scripts/MainGameplay/Spawn/PlayerSpawn.cs
+++ Assets/Scripts/MainGameplay/Spawn/PlayerSpawn.cs
@@ -7,9 +7,30 @@ public class PlayerSpawn : MonoBehaviour
 {
     [SerializeField] private GameObject _playerPrefab;
     GameObject _player;
+    private PhotonView pv;
+
+    private void Awake()
+    {
+        pv = GetComponent<PhotonView>();
+    }
 
     private void Start()
     {
         _player = PhotonNetwork.Instantiate(_playerPrefab.name, new Vector2(Random.Range(4, -4), Random.Range(4, -4)), Quaternion.identity);
+
+        //int playerIndex = PhotonNetwork.PlayerList.Length;
+
+        //pv.RPC("ChangeColor", RpcTarget.AllBuffered, _player.GetComponent<PhotonView>().ViewID, playerIndex);
     }
+
+    //[PunRPC]
+    //private void ChangeColor(int playerViewID, int playerIndex)
+    //{
+    //    PhotonView targetPhotonView = PhotonView.Find(playerViewID);
+
+    //    if (targetPhotonView != null ) 
+    //    {
+    //        targetPhotonView.gameObject.GetComponent<SpriteRenderer>().color = (playerIndex == 1) ? Color.red : Color.blue;
+    //    }
+    //}
 }
