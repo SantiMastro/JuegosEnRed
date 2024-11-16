9a3f41907bb9e44a294bdb41ba61e3edab69fd19
luqui1004 <lucascoceres0@gmail.com>
2024-11-16 17:27:28 -0300

    EXPERIMENTAL 12
    Animacion agregada a los personajes con el Photon Animator View

--- Assets/Scripts/MainGameplay/Player/PlayerController.cs
+++ Assets/Scripts/MainGameplay/Player/PlayerController.cs
@@ -8,11 +8,13 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private int _speed = 5;
     private PhotonView pv;
     private Camera _camera;
+    private Animator _animator;
 
     private void Awake()
     {
         pv = GetComponent<PhotonView>();
         _camera = GetComponentInChildren<Camera>();
+        _animator = GetComponent<Animator>();
     }
 
     private void Start()
@@ -30,21 +32,28 @@ public class PlayerController : MonoBehaviour
 
     private void Move()
     {
+        int direction = 0;
+
         if (Input.GetKey(KeyCode.W))
         {
             transform.position += Vector3.up * _speed * Time.deltaTime;
+            direction = 1;
         }
         if (Input.GetKey(KeyCode.S))
         {
             transform.position += -Vector3.up * _speed * Time.deltaTime;
+            direction = -1;
         }
         if (Input.GetKey(KeyCode.A))
         {
             transform.position += -Vector3.right * _speed * Time.deltaTime;
+            direction = -2;
         }
         if (Input.GetKey(KeyCode.D))
         {
             transform.position += Vector3.right * _speed * Time.deltaTime;
+            direction = 2;
         }
+        _animator.SetInteger("MovementDirection", direction);
     }
 }
